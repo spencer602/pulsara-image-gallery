4408e14e09b8ead74dfcd181038a03549a67053a
Spencer DeBuf <spencer602@gmail.com>
2020-06-24 11:08:37 -0600

    Initial Commit

---
+++ Pulsara Image Gallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pulsara Image Gallery
+//
+//  Created by Spencer DeBuf on 6/24/20.
+//  Copyright © 2020 Spencer DeBuf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
