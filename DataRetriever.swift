953600603e9cb07963a1f02bda1873ffa144119d
Spencer DeBuf <spencer602@gmail.com>
2020-06-24 11:57:45 -0600

    created data retriever class

---
+++ DataRetriever.swift
@@ -0,0 +1,13 @@
+//
+//  DataRetriever.swift
+//  Pulsara Image Gallery
+//
+//  Created by Spencer DeBuf on 6/24/20.
+//  Copyright © 2020 Spencer DeBuf. All rights reserved.
+//
+
+import Foundation
+
+class DataRetriever {
+    
+}
